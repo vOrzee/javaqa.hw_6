e0b6a3cedb219ec8854c79ceb2fbf4f79bb8c3d7
Roman Leshin <vorzee@gmail.com>
2024-08-25 09:25:50 +0500

    Инициализируем проект в соответствии с данными из предусловия

---
+++ src/main/java/ru/netology/services/MonthStatsService.java
@@ -0,0 +1,35 @@
+package ru.netology.services;
+
+public class MonthStatsService {
+
+    public int sumOfAllSales(int[] monthSales) {
+        //TODO
+        return 0;
+    }
+
+    public int avgOfSumSalesInMonth(int[] monthSales) {
+        //TODO
+        return 0;
+    }
+
+    public int numberMonthOfMaxSales(int[] monthSales) {
+        //TODO
+        return 0;
+    }
+
+    public int numberMonthOfMinSales(int[] monthSales) {
+        //TODO
+        return 0;
+    }
+
+    public int countMonthInWhichSalesWasBelowAverage(int[] monthSales) {
+        //TODO
+        return 0;
+    }
+
+    public int countMonthInWhichSalesWasAboveAverage(int[] monthSales) {
+        //TODO
+        return 0;
+    }
+
+}
